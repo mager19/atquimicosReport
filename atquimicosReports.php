00b318fc6a4593fc0277888c4ce991bd42aea564
mager19 <mager19@gmail.com>
2024-10-28 20:27:24 -0500

    change version

--- atquimicosReports.php
+++ atquimicosReports.php
@@ -4,7 +4,7 @@
  * Plugin Name: AtquimicosReports
  * Plugin URI: https://www.wordpress.org/mv-translations
  * Description: Plugin generate client reports for Atquimicos
- * Version: 1.0.6
+ * Version: 1.0.7
  * Requires at least: 5.6
  * Requires PHP: 7.0
  * Author: Mario Reyes C
